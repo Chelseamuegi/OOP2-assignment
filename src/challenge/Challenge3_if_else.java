41f9ff9c11ec76614e3417cf73cd117e6d4aee4b
Chelseamuegi <chelsea.muegi@strathmore.edu>
2025-04-29 12:50:07 +0300

    Update and rename RandomExample.java to Challenge3_if_else.java

---
+++ src/challenge/Challenge3_if_else.java
@@ -0,0 +1,19 @@
+package challenge;
+public class GradeChecker {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 90) {
+            System.out.println("Grade: A");
+        } else if (score >= 80) {
+            System.out.println("Grade: B");
+        } else if (score >= 70) {
+            System.out.println("Grade: C");
+        } else if (score >= 60) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F (Fail)");
+        }
+    }
+}
+
